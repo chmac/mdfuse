5e51be3e8e4868991cb72093480d2435e2096020
Callum Macdonald <git@callum-macdonald.com>
2022-04-11 14:55:04 +0200

    Add frontmatter to deps.

--- packages/mdfuse-cli/deps.ts
+++ packages/mdfuse-cli/deps.ts
@@ -1,2 +1,3 @@
 export * as fs from "https://deno.land/std@0.134.0/fs/mod.ts";
 export * as Cliffy from "https://deno.land/x/cliffy@v0.23.0/mod.ts";
+export * as frontmatter from "https://deno.land/x/frontmatter@v0.1.2/mod.ts";
