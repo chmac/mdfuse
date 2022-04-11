fd7047a6c36566d405eb9c81853761c52ece175d
Callum Macdonald <git@callum-macdonald.com>
2022-04-11 15:33:04 +0200

    Switch to my patched version of frontmatter.

--- packages/mdfuse-cli/deps.ts
+++ packages/mdfuse-cli/deps.ts
@@ -1,4 +1,5 @@
 export * as fs from "https://deno.land/std@0.134.0/fs/mod.ts";
 export * as Cliffy from "https://deno.land/x/cliffy@v0.23.0/mod.ts";
-export * as frontmatter from "https://deno.land/x/frontmatter@v0.1.2/mod.ts";
+// export * as frontmatter from "https://deno.land/x/frontmatter@v0.1.2/mod.ts";
+export * as frontmatter from "https://raw.githubusercontent.com/chmac/frontmatter/371b2b8afcb736903ab1c8b4355829ceb519486e/mod.ts";
 export { split } from "https://raw.githubusercontent.com/chmac/util-md-to-arrays/2bdd5cea16e1e400234244258a65ee8afa552994/src/index.ts";
