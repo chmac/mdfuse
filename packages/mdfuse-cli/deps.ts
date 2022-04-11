bf9cda37aee1cda6b8393f7ed9a55734d01255d6
Callum Macdonald <git@callum-macdonald.com>
2022-04-11 15:02:44 +0200

    Add split to deps.

--- packages/mdfuse-cli/deps.ts
+++ packages/mdfuse-cli/deps.ts
@@ -1,3 +1,4 @@
 export * as fs from "https://deno.land/std@0.134.0/fs/mod.ts";
 export * as Cliffy from "https://deno.land/x/cliffy@v0.23.0/mod.ts";
 export * as frontmatter from "https://deno.land/x/frontmatter@v0.1.2/mod.ts";
+export { split } from "https://raw.githubusercontent.com/chmac/util-md-to-arrays/2bdd5cea16e1e400234244258a65ee8afa552994/src/index.ts";
