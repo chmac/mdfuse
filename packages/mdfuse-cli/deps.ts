52611f3edada10522799465829eb74c8434b09fd
Callum Macdonald <git@callum-macdonald.com>
2022-04-11 15:52:07 +0200

    Add fuse to deps.

--- packages/mdfuse-cli/deps.ts
+++ packages/mdfuse-cli/deps.ts
@@ -3,3 +3,6 @@ export * as Cliffy from "https://deno.land/x/cliffy@v0.23.0/mod.ts";
 // export * as frontmatter from "https://deno.land/x/frontmatter@v0.1.2/mod.ts";
 export * as frontmatter from "https://raw.githubusercontent.com/chmac/frontmatter/371b2b8afcb736903ab1c8b4355829ceb519486e/mod.ts";
 export { split } from "https://raw.githubusercontent.com/chmac/util-md-to-arrays/2bdd5cea16e1e400234244258a65ee8afa552994/src/index.ts";
+// @deno-types="https://deno.land/x/fuse@v6.4.1/dist/fuse.d.ts"
+export { default as Fuse } from "https://deno.land/x/fuse@v6.4.1/dist/fuse.esm.min.js";
+export * as lodash from "https://deno.land/x/lodash@4.17.19/dist/lodash.fp.js";
