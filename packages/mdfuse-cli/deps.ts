67bb3e13233fbc7216dd23267a86008f84bd1e0a
Callum Macdonald <git@callum-macdonald.com>
2023-01-21 13:05:55 +0100

    Upgrade dependency for bugfix.

--- packages/mdfuse-cli/deps.ts
+++ packages/mdfuse-cli/deps.ts
@@ -4,7 +4,7 @@ export * as Cliffy from "https://deno.land/x/cliffy@v0.23.0/mod.ts";
 import { default as Debug } from "https://deno.land/x/debuglog/debug.ts";
 // export * as frontmatter from "https://deno.land/x/frontmatter@v0.1.2/mod.ts";
 export * as frontmatter from "https://raw.githubusercontent.com/chmac/frontmatter/371b2b8afcb736903ab1c8b4355829ceb519486e/mod.ts";
-export { split } from "https://raw.githubusercontent.com/chmac/util-md-to-arrays/2bdd5cea16e1e400234244258a65ee8afa552994/src/index.ts";
+export { split } from "https://raw.githubusercontent.com/chmac/util-md-to-arrays/bdc5ace0d60646c87393040de01fe60bf948c41a/src/index.ts";
 // @deno-types="https://deno.land/x/fuse@v6.4.1/dist/fuse.d.ts"
 export { default as Fuse } from "https://deno.land/x/fuse@v6.4.1/dist/fuse.esm.min.js";
 
