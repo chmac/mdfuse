3bd2907e7f9634b9413120fde91f41412897cdbb
Callum Macdonald <git@callum-macdonald.com>
2025-04-17 08:38:57 +0200

    Bump and fix frontmatter.

--- packages/mdfuse-cli/deps.ts
+++ packages/mdfuse-cli/deps.ts
@@ -3,7 +3,7 @@ export * as colours from "https://deno.land/std@0.134.0/fmt/colors.ts";
 export * as Cliffy from "https://deno.land/x/cliffy@v0.23.0/mod.ts";
 import { default as Debug } from "https://deno.land/x/debuglog/debug.ts";
 // export * as frontmatter from "https://deno.land/x/frontmatter@v0.1.2/mod.ts";
-export * as frontmatter from "https://raw.githubusercontent.com/chmac/frontmatter/371b2b8afcb736903ab1c8b4355829ceb519486e/mod.ts";
+export * as frontmatter from "https://raw.githubusercontent.com/chmac/frontmatter/f178dbbd82664b03e7a74dfb27c374768e2dde2d/mod.ts";
 export { split } from "https://raw.githubusercontent.com/chmac/util-md-to-arrays/bdc5ace0d60646c87393040de01fe60bf948c41a/src/index.ts";
 // @deno-types="https://deno.land/x/fuse@v6.4.1/dist/fuse.d.ts"
 export { default as Fuse } from "https://deno.land/x/fuse@v6.4.1/dist/fuse.esm.min.js";
